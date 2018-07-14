90cf0dd9363082eafb31cc8837623e493baee0ea
Wojtek Dudek <wdudek82@gmail.com>
2018-07-14 18:39:26 +0200

    Added: limit selectable cards to 2; deactivete selected cards only if second selected (after timeout)

--- src/App.js
+++ src/App.js
@@ -19,45 +19,84 @@ class App extends React.Component<Props, State> {
         id: '1',
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Kitten_in_Rizal_Park%2C_Manila.jpg/1200px-Kitten_in_Rizal_Park%2C_Manila.jpg',
+        isActive: false,
       },
       {
         id: '2',
         image:
           'https://www.godspeedanimalcare.com/images/content/rehabilitation-center/doggie-playcare.jpg',
+        isActive: false,
       },
       {
         id: '3',
         image:
           'https://www.zoopraha.cz/images/stavby-expozice/Ostrov_lemuru.jpg',
+        isActive: false,
       },
       {
         id: '4',
         image:
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbhh3OAq-MEt992I4kTute1hd8FUsc1cjqjKHOZrWkihw8M8aq',
+        isActive: false,
       },
       {
         id: '5',
         image:
           'http://cdn0.wideopenpets.com/wp-content/uploads/2018/02/Untitled-design-11-770x405.png',
+        isActive: false,
       },
     ],
   };
 
-  handleIncreateCardCounter = () => {
-    this.setState((prevState) => ({ activeCards: prevState.activeCards + 1 }));
+  componentDidUpdate() {
+    if (this.state.activeCards >= 2) {
+      setTimeout(this.deactivateAllCards, 2000);
+    }
+  }
+
+  deactivateAllCards = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      activeCards: 0,
+      cards: prevState.cards.map((card) => {
+        const updatedCard = card;
+        updatedCard.isActive = false;
+        return updatedCard;
+      }),
+    }));
   };
 
-  handleDecreaseCardCounter = () => {
-    this.setState((prevState) => ({ activeCards: prevState.activeCards - 1 }));
+  handleBoxActivation = (cardId: string) => {
+    const selectedCard = this.state.cards.filter(
+      (card) => card.id === cardId,
+    )[0];
+
+    if (this.state.activeCards >= 2 || selectedCard.isActive) {
+      return;
+    }
+
+    this.setState(
+      (prevState) => ({
+        ...prevState,
+        activeCards: prevState.activeCards + 1,
+        cards: prevState.cards.map((card) => {
+          const updatedCard = card;
+          if (updatedCard.id === cardId) {
+            updatedCard.isActive = true;
+          }
+          return updatedCard;
+        }),
+      })
+    );
   };
 
   renderCards = () => {
     return this.state.cards.map((card) => (
       <Square
         key={card.id}
+        clicked={() => this.handleBoxActivation(card.id)}
         noActivated={this.state.activeCards}
-        increaseCounter={this.handleIncreateCardCounter}
-        decreaseCounter={this.handleDecreaseCardCounter}
+        activated={card.isActive}
         front={card.id}
         image={card.image}
       />
@@ -68,9 +107,7 @@ class App extends React.Component<Props, State> {
     return (
       <Wrapper>
         <h1>Find the Difference</h1>
-        <Container>
-          {this.renderCards()}
-        </Container>
+        <Container>{this.renderCards()}</Container>
       </Wrapper>
     );
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -5,59 +5,29 @@ import { Wrapper } from './Square.styles';
 type Props = {
   front: string,
   image: string,
-  noActivated: number,
-  increaseCounter: (void) => void,
-  decreaseCounter: (void) => void,
-};
-
-type State = {
   activated: boolean,
+  clicked: (void) => void,
 };
 
-class Square extends React.Component<Props, State> {
-  state = {
-    activated: false,
-  };
+// type State = {
+//   activated: boolean,
+// };
 
-  handleBoxActivation = () => {
-    if (this.props.noActivated >= 2 || this.state.activated) {
-      return;
-    }
-
-    this.props.increaseCounter();
-
-    this.setState(
-      () => ({ activated: true }),
-      () => setTimeout(() => {
-        this.props.decreaseCounter();
-        this.setState(() => ({ activated: false }));
-      }, 1000),
-    );
-  };
-
-  handleBoxDeactivation = () => {
-    this.setState(
-      () => ({ activated: false }),
-      () => setTimeout(() => this.props.decreaseCounter(), 1000),
-    );
-  };
-
-  render() {
-    return (
-      <Wrapper
-        onClick={this.handleBoxActivation}
-        activated={this.state.activated}
-        bgImg={this.props.image}
-      >
-        <div className="card">
-          <div className="face front">
-            <h4>{this.props.front}</h4>
-          </div>
-          <div className="face back" />
+const Square = (props: Props) => {
+  return (
+    <Wrapper
+      onClick={props.clicked}
+      activated={props.activated}
+      bgImg={props.image}
+    >
+      <div className="card">
+        <div className="face front">
+          <h4>{props.front}</h4>
         </div>
-      </Wrapper>
-    );
-  }
-}
+        <div className="face back" />
+      </div>
+    </Wrapper>
+  );
+};
 
 export default Square;
